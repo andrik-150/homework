454354eff38e615d3728372ae8a7f028bbf40f11
Андрей <andrik150@rambler.ru>
2022-08-08 16:37:45 +0300

    Task6

---
+++ Task/Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+
+int number =int.Parse(Console.ReadLine());
+
+if (number % 2 ==1)
+
+Console.WriteLine("Число " + number + " является: НЕЧЁТНЫМ");
+ else   
+Console.WriteLine("Число " + number + " является: ЧЁТНЫМ");

f0de3fe8d66d6a8b5be394dc4d1713d2bc2132e3
Андрей <andrik150@rambler.ru>
2022-08-08 15:42:57 +0300

    Task4

---
+++ Task/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите три числа: ");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+int c=int.Parse(Console.ReadLine());
+int max=a;
+if (a> max ) max = a;
+if (b> max ) max = b;
+if (c> max ) max = c;
+Console.Write("max=");
+Console.Write(max);

7e607824f9d1fbae324b6509a957ee08e27fe331
andrik-150 <109854129+andrik-150@users.noreply.github.com>
2022-08-10 17:28:15 +0300

    Update Program.cs

--- Task/Task2/Program.cs
+++ Task/Task2/Program.cs
@@ -1,9 +1,7 @@
-﻿Console.WriteLine("Введите два числа: ");
-int a=int.Parse(Console.ReadLine());
-int b=int.Parse(Console.ReadLine());
-int max=a;
-if (a>b)
-if (a> max ) max = a;
-if (b> max ) max = b;
-Console.Write("max=");
-Console.Write(max);
+Console.WriteLine("Введите два числа ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+Console.Write($"max = {max}");
